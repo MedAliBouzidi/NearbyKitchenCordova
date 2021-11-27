25c243a1f97e83249107d0d88a7bb518fc5cf4d2
BOUZIDI Med Ali <med23908@gmail.com>
2021-11-27 17:41:01 +0100

    Fix css for Sign up page

--- www/js/index.js
+++ www/js/index.js
@@ -21,44 +21,42 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 var typeConnexion;
 
-(function (){
+(function() {
     "use strict";
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
 
-      document.addEventListener('online', onOnline,false);
-      document.addEventListener('offline',onOffline,false);
-      checkConnection();
+        document.addEventListener('online', onOnline, false);
+        document.addEventListener('offline', onOffline, false);
+        //checkConnection();
     };
 
     function checkConnection() {
         var connectiontype = navigator.connection.type;
-    
+
         var status = {};
-        status[Connection.UNKNOWN]  = 'Unknown connection';
+        status[Connection.UNKNOWN] = 'Unknown connection';
         status[Connection.ETHERNET] = 'Ethernet connection';
-        status[Connection.WIFI]     = 'WiFi connection';
-        status[Connection.CELL_2G]  = 'Cell 2G connection';
-        status[Connection.CELL_3G]  = 'Cell 3G connection';
-        status[Connection.CELL_4G]  = 'Cell 4G connection';
-        status[Connection.CELL]     = 'Cell generic connection';
-        status[Connection.NONE]     = 'No network connection';
-    
+        status[Connection.WIFI] = 'WiFi connection';
+        status[Connection.CELL_2G] = 'Cell 2G connection';
+        status[Connection.CELL_3G] = 'Cell 3G connection';
+        status[Connection.CELL_4G] = 'Cell 4G connection';
+        status[Connection.CELL] = 'Cell generic connection';
+        status[Connection.NONE] = 'No network connection';
+
         alert('Connection type: ' + status[connectiontype]);
     }
 
 
-    function onOnline()
-    {
-     $("#infos").html("Connexion établie de type : " + connectiontype);
+    function onOnline() {
+        $("#infos").html("Connexion établie de type : " + connectiontype);
     }
-    function onOffline()
-    {
-      $("#infos").html("Vérifier votre connexion !");
+
+    function onOffline() {
+        $("#infos").html("Vérifier votre connexion !");
     }
-    
+
 
 })();
-
