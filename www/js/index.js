50a29f55e6d6a952d6295be8c64097db7983c8b0
BOUZIDI Med Ali <med23908@gmail.com>
2021-11-28 02:08:22 +0100

    Add TODOs

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-var typeConnexion;
-
-(function() {
+(function () {
     "use strict";
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
@@ -31,7 +8,7 @@ var typeConnexion;
         document.addEventListener('online', onOnline, false);
         document.addEventListener('offline', onOffline, false);
         //checkConnection();
-    };
+    }
 
     function checkConnection() {
         var connectiontype = navigator.connection.type;
@@ -49,7 +26,6 @@ var typeConnexion;
         alert('Connection type: ' + status[connectiontype]);
     }
 
-
     function onOnline() {
         $("#infos").html("Connexion établie de type : " + connectiontype);
     }
@@ -58,5 +34,9 @@ var typeConnexion;
         $("#infos").html("Vérifier votre connexion !");
     }
 
-
 })();
+
+/* TODO:
+  return onLine variable when device is online and the same when device is offline
+  use that variable to limit user control in the app
+*/
