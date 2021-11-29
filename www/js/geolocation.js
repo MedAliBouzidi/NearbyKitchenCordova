b52198d1249a5dabe6f432e48d13a6df0d5828b0
BOUZIDI Med Ali <med23908@gmail.com>
2021-11-29 23:47:50 +0100

    Fix Search map Bug

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -1,30 +1,27 @@
-navigator.geolocation.getCurrentPosition(onSuccess, onerror, {
-    enableHighAccuracy: false,
-    maximumAge: 0,
-    timeout: 4000
-});
+initMap()
+
+function initMap() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
 
 function onSuccess(position) {
-    alert("hey");
-    var mapCanvas = document.getElementById('map-canvas');
-    var lat = position.coords.latitude;
-    var lang = position.coords.longitude;
-    var myLatlng = new google.maps.LatLng(lat, lang);
-    var mapOptions = {
-        zoom: 4,
-        center: myLatlng
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map
-    });
+    const mapCanvas = document.getElementById('map-canvas')
+
+    let lat = position.coords.latitude
+    let long = position.coords.longitude
+
+    let latLong = new google.maps.LatLng(lat, long)
+
+    let map = new google.maps.Map(
+        mapCanvas,
+        {
+            center: latLong,
+            zoom: 8
+        }
+    )
+
 }
 
 function onError(error) {
-    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n')
 }
-
-var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 3000 });
-
-google.maps.event.addDomListener(window, 'load', onSuccess);
